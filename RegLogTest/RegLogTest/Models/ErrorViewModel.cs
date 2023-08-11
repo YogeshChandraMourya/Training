75d8bad89372ed2c7146be50b1d907de23869445
Yogesh Chandra Mourya Banala <ybanala@evoketechnologies.com>
2023-08-11 16:06:07 +0530

    RegLogTest

---
+++ RegLogTest/RegLogTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RegLogTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
